9d938464093b95a79ae79e692401430bf44f8a5b
Yeradis P. Barbosa Marrero <yeradis@gmail.com>
2017-09-17 21:21:56 +0200

    Item layout update... and now fetch online images

--- anime_tracking/lib/scenes/anime/anime_list_item.dart
+++ anime_tracking/lib/scenes/anime/anime_list_item.dart
@@ -22,39 +22,42 @@ class AnimeListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    Image coverSection = new Image.asset(
-      'lib/assets/drawer_header_background.png',
-      height: 128.0,
-      fit: BoxFit.fitWidth,
+    NetworkImage networkImage = new NetworkImage(item.cover);
+
+    Image coverImage = new Image(image: networkImage,
+      height: 128.0,fit: BoxFit.fitWidth);
+
+    Container title = new Container(
+      padding: const EdgeInsets.only(bottom: 4.0),
+      child: new Text(
+        item.name,
+        style: new TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
 
-    Row titleSection = new Row(
+    Text subtitle = new Text(
+      item.plot,
+      style: new TextStyle(
+        color: Colors.grey,
+      ),
+    );
+
+    Row infoSection = new Row(
         children: [
           new Expanded(
             child: new Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                new Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: new Text(
-                    item.name,
-                    style: new TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                new Text(
-                  item.plot,
-                  style: new TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
+                title,
+                subtitle,
               ],
             ),
           ),
           new Icon(
             Icons.star,
-            color: Colors.red[500],
+            color: Colors.redAccent,
           ),
           new Text(item.rating),
         ],
@@ -62,10 +65,10 @@ class AnimeListItem extends StatelessWidget {
 
     ListView content = new ListView(
       children: [
-        coverSection,
+        coverImage,
         new Container(
-          margin: const EdgeInsets.all(8.0),
-          child: titleSection,
+          margin: const EdgeInsets.all(10.0),
+          child: infoSection,
           ),
       ],
     );
